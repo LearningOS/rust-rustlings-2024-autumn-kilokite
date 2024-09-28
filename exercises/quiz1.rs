76499658fec656f491a60292cae44094143d5a2c
kilokite <kilokiter@outlook.com>
2024-09-28 22:49:34 +0800

    update a

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -31,7 +31,6 @@ fn verify_test() {
     let price2 = calculate_price_of_apples(40);
     let price3 = calculate_price_of_apples(41);
     let price4 = calculate_price_of_apples(65);
-
     assert_eq!(70, price1);
     assert_eq!(80, price2);
     assert_eq!(41, price3);
